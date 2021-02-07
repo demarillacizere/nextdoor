ea0fbb64cf2e7871e70bccaa50516001f6699bc2
Demarillac Izere <izered3@gmail.com>
2021-02-07 17:57:55 +0200

    updated models in the admin page

--- nextdoor/admin.py
+++ nextdoor/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Neighborhood,Profile,Alert,Hospital,Business
+from .models import Neighborhood,Profile,Business
 
 admin.site.register(Neighborhood)
 admin.site.register(Alert)
 admin.site.register(Profile)
-admin.site.register(Business)
-admin.site.register(Hospital)
+
 
